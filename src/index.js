dafe42182553580269a14f57953dcd5fd1de5b31
bk <bk@diffent.com>
2022-06-22 12:52:41 -0400

    try to start removing typescript, issues w/ legacy react native project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import { NativeModules } from 'react-native';
-import { Decimal } from 'decimal.js';
+import { NativeModules } from 'react-native'
+import { Decimal } from 'decimal.js'
 
+/*
 interface ImagesToVideoOptions {
   fileName: string;
   screenTimePerImage: number;
@@ -8,27 +9,30 @@ interface ImagesToVideoOptions {
   height: number;
   absolutePaths: Array<string>;
 }
+*/
 
+/*
 type ImagesToVideoType = {
   render(options: ImagesToVideoOptions): Promise<string>;
 };
+*/
 
 const { ImagesToVideo } = NativeModules;
 
 const precision = 1000;
 
 const ImagesToVideoHelper = {
-  render(options: ImagesToVideoOptions): Promise<string> {
-    const screenTimeDecimal = new Decimal(options.screenTimePerImage);
+  render(options) {
+    const screenTimeDecimal = new Decimal(options.screenTimePerImage)
 
-    const [timeValue, timeScale] = screenTimeDecimal.toFraction(precision);
+    const [timeValue, timeScale] = screenTimeDecimal.toFraction(precision)
 
     return ImagesToVideo.render({
       ...options,
       timeValue: timeValue.toNumber(),
       timeScale: timeScale.toNumber(),
-    });
-  },
-};
+    })
+  }
+}
 
-export default ImagesToVideoHelper as ImagesToVideoType;
+export default ImagesToVideoHelper
